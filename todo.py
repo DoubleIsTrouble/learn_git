a1b822e0b0d57988364c4f7f7d041f4e37a28914
cian_hu <cian_hu@sina.com>
2026-04-19 16:41:28 +0800

    modify

--- todo.py
+++ todo.py
@@ -1,10 +1,11 @@
 # TODO 1.cursor 中使用git
 # TODO 2.git tag
-# TODO 3.本地不同分支中是否看到的内容不同
+# TODO 3.本地不同分支中是否看到的内容不同 是的
 # TODO 4.git action
 # TODO 5.git worktree
 # TODO 6.冲突解决
 # TODO 7.Gitee
 # TODO 8.开源贡献
-# TODO 9.团队协作 git workflow 分支到底如何管理
+# TODO 9.团队协作 git workflow 分支到底如何管理 main pull request dev+feat
+# TODO 10.rebase and fast-forward
 ########
